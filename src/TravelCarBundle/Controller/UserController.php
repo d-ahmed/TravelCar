87e4e97e6523b2dce541dbf7b00f94b80c6b2686
lionda <daniel.ahmedinfo@gmail.com>
2017-03-12 13:19:04 +0100

    index vehicle

--- src/TravelCarBundle/Controller/UserController.php
+++ src/TravelCarBundle/Controller/UserController.php
@@ -2,11 +2,24 @@
 
 namespace TravelCarBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use TravelCarBundle\Entity\User;
 
 class UserController extends Controller
 {
+    /**
+     * @Route("user/edit/{id}", name="user_edit")
+     * @Method("GET")
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function editAction(User $user)
+    {
+        return $this->render('TravelCarBundle:Default:User/Layout/edit.html.twig', array("user"=>$user));
+    }
+
     /**
      * @Security("has_role('ROLE_ADMIN')")
      */
