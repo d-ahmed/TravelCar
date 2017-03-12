71d094dd576301c02799b1ab18c42a36f4e9aee9
AnasseZ <anasse-37@hotmail.fr>
2017-03-12 10:29:14 +0100

    Modif RSS/asyncrone/trads/ + actuellement profil page

--- web/assets/js/expose.js
+++ web/assets/js/expose.js
@@ -9,11 +9,18 @@ $(document).ready(function() {
                 $("#lastAdvert").children().remove();
                 $.each(response, function (i) {
                     var departureDate = new Date(response[i].departureTime);
+                    var departureCity = response[i].departureCity;
+                    var cityOfArrival = response[i].cityOfArrival;
+                    var places_availables = response[i].numberOfPlace - response[i].numberOfReservation;
+                    var id = response[i].id;
                     $("#lastAdvert").append(
-                        "<div class='col-md-3' >" +
+                        "<div class='col-md-4' >" +
                         "<div class='thumbnail'>" +
                         "<div class='caption'> " +
                         "<strong>"+departureDate.toLocaleDateString()+" à "+departureDate.toLocaleTimeString()+"</strong>"+
+                        "<p><span class=\"glyphicon glyphicon-road text-primary\"></span> "+ departureCity + " → "+ cityOfArrival +
+                        "<p><span class=\"glyphicon glyphicon-user text-primary\"></span> " + places_availables +  " places disponibles.</p>"+
+                        "<a href=\"\" class=\"btn btn-info\" type=\"button\"> Réserver </a>"+
                         "</div> " +
                         "</div>" +
                         " </div>"
